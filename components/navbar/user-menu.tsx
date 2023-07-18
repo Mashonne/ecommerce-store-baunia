b8c8a33b2e43a9c5a94733d883a1201a3cab2f22
Mashonne <himashcareem16@gmail.com>
2023-07-19 04:25:30 +0530

    fixed

--- components/navbar/user-menu.tsx
+++ components/navbar/user-menu.tsx
@@ -28,7 +28,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
   return (
     <>
       <div className="relative">
-        <div className="flex flex-row items-center gap-3">
+        <div className="flex flex-row items-center gap-2">
           <div
             onClick={toggleOpen}
             className="
