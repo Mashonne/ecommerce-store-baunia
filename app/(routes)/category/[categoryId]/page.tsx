bc37aec0a06bfee3d37a7001a368fb6e12ca3e0b
Mashonne <himashcareem16@gmail.com>
2023-07-25 08:18:46 +0530

    added friendly mobile responsiveness

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -48,7 +48,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
             </div>
             <div className="mt-6 lg:col-span-4 lg:mt-0">
               {products.length === 0 && <NoResults />}
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {products.map((item) => (
                   <div key={item.id}>
                     <ProductCard data={item} />
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -16,9 +16,7 @@ interface ProductCardProps {
   data: Product;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({ 
-  data 
-}) => {
+const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
   const cart = useCart();
   const router = useRouter();
   const previewModal = usePreviewModal();
@@ -27,24 +25,27 @@ const ProductCard: React.FC<ProductCardProps> = ({
     router.push(`/product/${data?.id}`);
   };
 
-  const onPreview :MouseEventHandler<HTMLButtonElement> = (event) =>{
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
     event.stopPropagation();
 
     previewModal.onOpen(data);
-  }
+  };
 
-  const onFavorite :MouseEventHandler<HTMLDivElement> = (event) =>{
+  const onFavorite: MouseEventHandler<HTMLDivElement> = (event) => {
     event.stopPropagation();
-  }
+  };
 
-  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) =>{
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
     event.stopPropagation();
 
-    cart.addItem(data, 1)
-  }
+    cart.addItem(data, 1);
+  };
 
   return (
-    <div onClick={handleClick} className="bg-white group p-3 border rounded-xl cursor-pointer space-y-4">
+    <div
+      onClick={handleClick}
+      className="bg-white group border rounded-xl cursor-pointer space-y-1"
+    >
       {/* Images and Actions */}
       <div
         className="
@@ -52,8 +53,8 @@ const ProductCard: React.FC<ProductCardProps> = ({
                     w-full 
                     h-full               
                     relative 
+                    rounded-t-xl
                     overflow-hidden 
-                    rounded-xl
                     bg-gray-100
                 "
       >
@@ -63,39 +64,43 @@ const ProductCard: React.FC<ProductCardProps> = ({
           src={data?.images?.[0]?.url}
           className="
                         object-cover
-                        rounded-md
+                        rounded-t-md
                         group-hover:scale-110
                         transition
                     "
         />
         <div onClick={onFavorite} className="absolute top-3 right-3">
-          <HeartButton 
-              productId={data.id} 
-          />
+          <HeartButton productId={data.id} />
         </div>
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
-          <div className="flex gap-x-6 justify-cneter">
-            <IconButton
-              onClick={onPreview}
-              icon={<Expand size={20} className="text-gray-600" />}
-            />
-            <IconButton
-              onClick={onAddToCart}
-              icon={<ShoppingCart size={20} className="text-gray-600" />}
-            />
+          <div className="flex gap-x-6 justify-center">
+            <div className="hidden sm:block">
+              <IconButton
+                onClick={onPreview}
+                icon={
+                  <Expand size={20} className="text-gray-600" />
+                }
+              />
+            </div>
+            <div>
+              <IconButton
+                onClick={onAddToCart}
+                icon={<ShoppingCart size={20} className="rounded-none text-gray-600" />}
+              />
+            </div>
           </div>
         </div>
       </div>
       {/* Description */}
-      <div className="flex flex-col gap-3">
-          <div>
-            <p className="font-semibold text-lg">{data.name}</p>
-            <p className="text-sm text-gray-500">{data.category?.name}</p>
-          </div>
-          {/* Price */}
-          <div className="flex items-center justify-between">
-            <Currency value={data?.price} />
-          </div>
+      <div className="flex flex-col p-3 gap-3">
+        <div>
+          <p className="font-semibold text-sm sm:text-lg">{data.name}</p>
+          <p className="hidden sm:block text-sm text-gray-500">{data.category?.name}</p>
+        </div>
+        {/* Price */}
+        <div className="flex items-center justify-between">
+          <Currency value={data?.price} />
+        </div>
       </div>
     </div>
   );
